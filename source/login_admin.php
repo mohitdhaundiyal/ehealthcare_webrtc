3e7d894ef40e45fbb4023bd6c942a1fb45ec2d22
mohitdhaundiyal <mohit.dhaundiyal22@gmail.com>
2020-03-17 13:40:48 +0530

    modified source

--- source/login_admin.php
+++ source/login_admin.php
@@ -1,24 +1,24 @@
 <?php
-session_start();
-include('../includes/connect.php');
-if(isset($_POST['submit'])){
-	$admin_name=$_POST['admin_name'];
-	$admin_password=($_POST['admin_password']);
-	$sql="SELECT * FROM admin where admin_name = '$admin_name' and admin_password ='$admin_password'";
-	//echo $sql; exit;
-	$result=mysqli_query($conn,$sql);
-	if($data=mysqli_num_rows($result) == 1){
-		$row=mysqli_fetch_assoc($result);
-		$_SESSION['admin_name']==$row['admin_name'];
-        $_SESSION['id']=$row['id'];
-		$status=1;
-		header("location:../admin/dashboard.php?status=".$status);
-		exit;
+	session_start();
+	include('../includes/connect.php');
+	if(isset($_POST['submit'])){
+		$admin_name=$_POST['admin_name'];
+		$admin_password=($_POST['admin_password']);
+		$sql="SELECT * FROM admin where admin_name = '$admin_name' and admin_password ='$admin_password'";
+		//echo $sql; exit;
+		$result=mysqli_query($conn,$sql);
+		if($data=mysqli_num_rows($result) == 1){
+			$row=mysqli_fetch_assoc($result);
+			$_SESSION['admin_name']==$row['admin_name'];
+			$_SESSION['id']=$row['id'];
+			$status=1;
+			header("location:../admin/dashboard.php?status=".$status);
+			exit;
+		}
+		else{
+			$status="2";
+			header("location:../admin_login.php?status=".$status);
+			echo "INVALID USER";
+		}
 	}
-	else{
-		$status="2";
-	header("location:../admin_login.php?status=".$status);
-	echo "INVALID USER";
-	}
-}
 ?>
--- source/login_doctor.php
+++ source/login_doctor.php
@@ -1,24 +1,24 @@
 <?php
-session_start();
-include('../includes/connect.php');
-if(isset($_POST['submit'])){
-	$doctor_email=$_POST['doctor_email'];
-	$doctor_password=md5($_POST['doctor_password']);
-	$sql="SELECT * FROM doctor where doctor_email = '$doctor_email' and doctor_password ='$doctor_password'";
-	//echo $sql;exit;
-	$result=mysqli_query($conn,$sql);
-	if($data=mysqli_num_rows($result) == 1){
-		$row=mysqli_fetch_assoc($result);
-		$_SESSION['doctor_name']==$row['doctor_name'];
-        $_SESSION['id']=$row['id'];
-		$status=1;
-		header("location:../doctor_dashboard.php?id=".$_SESSION['id']);
-		exit;
+	session_start();
+	include('../includes/connect.php');
+	if(isset($_POST['submit'])){
+		$doctor_email=$_POST['doctor_email'];
+		$doctor_password=md5($_POST['doctor_password']);
+		$sql="SELECT * FROM doctor where doctor_email = '$doctor_email' and doctor_password ='$doctor_password'";
+		//echo $sql;exit;
+		$result=mysqli_query($conn,$sql);
+		if($data=mysqli_num_rows($result) == 1){
+			$row=mysqli_fetch_assoc($result);
+			$_SESSION['doctor_name']==$row['doctor_name'];
+			$_SESSION['id']=$row['id'];
+			$status=1;
+			header("location:../doctor_dashboard.php?id=".$_SESSION['id']);
+			exit;
+		}
+		else{
+			$status="2";
+			header("location:../doctor_login.php?status=".$status);
+			echo "INVALID USER";
+		}
 	}
-	else{
-		$status="2";
-	header("location:../doctor_login.php?status=".$status);
-	echo "INVALID USER";
-	}
-}
 ?>
